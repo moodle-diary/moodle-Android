b5ebba2c5005ec3b4f27afd7230daa2f99fd0897
yeonsu <jack0810@kookmin.ac.kr>
2023-11-13 22:13:58 +0900

    refactor : refactor component position

--- app/src/main/java/eu/tutorial/moodle/ui/calendar/CalendarCard.kt
+++ app/src/main/java/eu/tutorial/moodle/ui/calendar/CalendarCard.kt
@@ -1,7 +1,6 @@
 package eu.tutorial.moodle.ui.calendar
 
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
@@ -20,10 +19,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,10 +28,9 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import eu.tutorial.moodle.R
-import eu.tutorial.moodle.ui.component.CalendarMonthItem
-import eu.tutorial.moodle.ui.component.DayOfWeek
+import eu.tutorial.moodle.ui.calendar.component.CalendarMonthItem
+import eu.tutorial.moodle.ui.calendar.component.DayOfWeek
 import eu.tutorial.moodle.ui.home.HomeViewModel
-import kotlinx.coroutines.withContext
 import java.time.LocalDate
 import java.time.YearMonth
 import java.util.Locale
