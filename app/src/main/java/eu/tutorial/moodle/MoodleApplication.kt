64065e3fb048b3484cf6ae47a15f8dfa6df8baa6
yeonsu <jack0810@kookmin.ac.kr>
2023-11-22 22:52:01 +0900

    refactor: packages

--- app/src/main/java/eu/tutorial/moodle/MoodleApplication.kt
+++ app/src/main/java/eu/tutorial/moodle/MoodleApplication.kt
@@ -1,6 +1,7 @@
 package eu.tutorial.moodle
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import eu.tutorial.moodle.data.AppContainer
 import eu.tutorial.moodle.data.AppDataContainer
 
@@ -10,6 +11,8 @@ class MoodleApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         container = AppDataContainer(this)
+
+        FirebaseApp.initializeApp(this)
     }
 
 
